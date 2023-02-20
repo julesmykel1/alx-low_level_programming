d18f985cb21e2bb1078ceebdf229d39377d4a6aa
julesmykel1 <julesukonu@gmail.com>
2023-02-19 18:21:17 -0800

    my variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry a number to the variable n and print character.
+ *
+ * Description: Prints the alphabet in lowercase.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+  
+{
+  int c;
+  for (c = '0' ; c <= '9' ; c++)
+    {
+      putchar(c); 
+    }
+  putchar('\n');
+  return (0); 
+}
